b0c4564153a465e9f54dd3912da9bc53822e65f3
Track <chevaldonnethubert@gmail.com>
2025-03-21 15:55:08 +0100

    Start implementing the jumping part

--- main.lua
+++ main.lua
@@ -1,74 +1,76 @@
 function love.load()
-    WALKING0 = love.graphics.newImage("Resources/Walking/Walking0.png")
-    WALKING1 = love.graphics.newImage("Resources/Walking/Walking1.png")
-    WALKING2 = love.graphics.newImage("Resources/Walking/Walking2.png")
-    WALKING3 = love.graphics.newImage("Resources/Walking/Walking3.png")
-    WALKING4 = love.graphics.newImage("Resources/Walking/Walking4.png")
-    WALKING5 = love.graphics.newImage("Resources/Walking/Walking5.png")
-    WALKING6 = love.graphics.newImage("Resources/Walking/Walking6.png")
-    WALKING7 = love.graphics.newImage("Resources/Walking/Walking7.png")
-    WALKING8 = love.graphics.newImage("Resources/Walking/Walking8.png")
-    WALKING9 = love.graphics.newImage("Resources/Walking/Walking9.png")
+    require("resources")
+    load_resources()
+    CHARACTER_X = 10
+    CHARACTER_Y = 320
+    CHARACTER_STATE = "walking"
     WALKING_ANIMATION = {WALKING0, WALKING1, WALKING2, WALKING3, WALKING4, WALKING5, WALKING6, WALKING7, WALKING8, WALKING9}
+    JUMPING_ANIMATION = {JUMPING0, JUMPING1, JUMPING2, JUMPING3, JUMPING4, JUMPING5, JUMPING6, JUMPING7, JUMPING8}
     ACTUAL_FRAME = WALKING_ANIMATION[1]
     CURRENT_WALKING_ANIMATION = 1
+    CURRENT_JUMPING_ANIMATION = 1
     ANIMATION_TIME = 0
-    ANIMATION_LENGTH = 2/60
+    ANIMATION_LENGTH_WALKING = 2/60
+    ANIMATION_LENGTH_JUMPING = 4/60
+    JUMPING_DELAY = 2/60
+    DELAY_TIME = 0
+    JUMPING_MINIMAL_TIME = 15/60
+    JUMPING_TIME = 0
 end
-
--- function Walking_animation()
---     love.graphics.clear()
---     love.graphics.draw(WALKING0)
---     love.timer.sleep(0.5)
---     love.graphics.clear()
---     love.graphics.draw(WALKING1)
---     love.timer.sleep(0.5)
---     love.graphics.clear()
---     love.graphics.draw(WALKING2)
---     love.timer.sleep(0.5)
---     love.graphics.clear()
---     love.graphics.draw(WALKING3)
---     love.timer.sleep(0.5)
---     love.graphics.clear()
---     love.graphics.draw(WALKING4)
---     love.timer.sleep(0.5)
---     love.graphics.clear()
---     love.graphics.draw(WALKING5)
---     love.timer.sleep(0.5)
---     love.graphics.clear()
---     love.graphics.draw(WALKING6)
---     love.timer.sleep(0.5)
---     love.graphics.clear()
---     love.graphics.draw(WALKING7)
---     love.timer.sleep(0.5)
---     love.graphics.clear()
---     love.graphics.draw(WALKING8)
---     love.timer.sleep(0.5)
---     love.graphics.clear()
---     love.graphics.draw(WALKING9)
---     love.timer.sleep(0.5)
--- end
 
 function love.update(dt)
     if dt < 1/60 then
         love.timer.sleep(1/60 - dt)
     end
-    ANIMATION_TIME = ANIMATION_TIME + dt
-    if ANIMATION_TIME >= ANIMATION_LENGTH then
-        CURRENT_WALKING_ANIMATION = CURRENT_WALKING_ANIMATION + 1
-        if CURRENT_WALKING_ANIMATION > #WALKING_ANIMATION then
-            CURRENT_WALKING_ANIMATION = 1
+    if CHARACTER_STATE == "jumping" then
+        JUMPING_TIME = JUMPING_TIME + dt
+    end
+    if love.keyboard.isDown("up")then
+        DELAY_TIME = DELAY_TIME + dt
+        if DELAY_TIME >= JUMPING_DELAY then
+            CHARACTER_STATE = "jumping"
+            DELAY_TIME = 0
         end
-        ACTUAL_FRAME = WALKING_ANIMATION[CURRENT_WALKING_ANIMATION]
-        ANIMATION_TIME = 0
-        if ANIMATION_LENGTH == 2/60 then
-            ANIMATION_LENGTH = 3/60
-        else
-            ANIMATION_LENGTH = 2/60
+    else
+        if JUMPING_TIME >= JUMPING_MINIMAL_TIME then
+            CHARACTER_STATE = "walking"
+        end
+    end
+    ANIMATION_TIME = ANIMATION_TIME + dt
+    if CHARACTER_STATE == "walking" then
+        if ANIMATION_TIME >= ANIMATION_LENGTH_WALKING then
+            CURRENT_WALKING_ANIMATION = CURRENT_WALKING_ANIMATION + 1
+            if CURRENT_WALKING_ANIMATION > #WALKING_ANIMATION then
+                CURRENT_WALKING_ANIMATION = 1
+            end
+            ACTUAL_FRAME = WALKING_ANIMATION[CURRENT_WALKING_ANIMATION]
+            ANIMATION_TIME = 0
+            if ANIMATION_LENGTH_WALKING == 2/60 then
+                ANIMATION_LENGTH_WALKING = 3/60
+            else
+                ANIMATION_LENGTH_WALKING = 2/60
+            end
+        end
+        CURRENT_JUMPING_ANIMATION = 1
+        ANIMATION_LENGTH_JUMPING = 4/60
+        JUMPING_TIME = 0
+    elseif CHARACTER_STATE == "jumping" then
+        if ANIMATION_TIME >= ANIMATION_LENGTH_JUMPING then
+            CURRENT_JUMPING_ANIMATION = CURRENT_JUMPING_ANIMATION + 1
+            if CURRENT_JUMPING_ANIMATION > #JUMPING_ANIMATION then
+                CURRENT_JUMPING_ANIMATION = 2
+            end
+            ACTUAL_FRAME = JUMPING_ANIMATION[CURRENT_JUMPING_ANIMATION]
+            ANIMATION_TIME = 0
+            if ANIMATION_LENGTH_JUMPING == 4/60 or ANIMATION_LENGTH_JUMPING == 2/60 then
+                ANIMATION_LENGTH_JUMPING = 3/60
+            else
+                ANIMATION_LENGTH_JUMPING = 2/60
+            end
         end
     end
 end
 
 function love.draw()
-    love.graphics.draw(ACTUAL_FRAME)
+    love.graphics.draw(ACTUAL_FRAME,CHARACTER_X,CHARACTER_Y)
 end
