eacfc2e860969e22d8396b9707694d3df98ff2c8
Track <chevaldonnethubert@gmail.com>
2025-03-21 15:54:45 +0100

    Add a resources file

---
+++ resources.lua
@@ -0,0 +1,29 @@
+function load_resources()
+    load_walking()
+    load_jumping()
+end
+
+function load_walking()
+    WALKING0 = love.graphics.newImage("Resources/Walking/Walking0.png")
+    WALKING1 = love.graphics.newImage("Resources/Walking/Walking1.png")
+    WALKING2 = love.graphics.newImage("Resources/Walking/Walking2.png")
+    WALKING3 = love.graphics.newImage("Resources/Walking/Walking3.png")
+    WALKING4 = love.graphics.newImage("Resources/Walking/Walking4.png")
+    WALKING5 = love.graphics.newImage("Resources/Walking/Walking5.png")
+    WALKING6 = love.graphics.newImage("Resources/Walking/Walking6.png")
+    WALKING7 = love.graphics.newImage("Resources/Walking/Walking7.png")
+    WALKING8 = love.graphics.newImage("Resources/Walking/Walking8.png")
+    WALKING9 = love.graphics.newImage("Resources/Walking/Walking9.png")
+end
+
+function load_jumping()
+    JUMPING0 = love.graphics.newImage("Resources/Jumping/Jumping0.png")
+    JUMPING1 = love.graphics.newImage("Resources/Jumping/Jumping1.png")
+    JUMPING2 = love.graphics.newImage("Resources/Jumping/Jumping2.png")
+    JUMPING3 = love.graphics.newImage("Resources/Jumping/Jumping3.png")
+    JUMPING4 = love.graphics.newImage("Resources/Jumping/Jumping4.png")
+    JUMPING5 = love.graphics.newImage("Resources/Jumping/Jumping5.png")
+    JUMPING6 = love.graphics.newImage("Resources/Jumping/Jumping6.png")
+    JUMPING7 = love.graphics.newImage("Resources/Jumping/Jumping7.png")
+    JUMPING8 = love.graphics.newImage("Resources/Jumping/Jumping8.png")
+end
